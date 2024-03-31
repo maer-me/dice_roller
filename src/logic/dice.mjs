0b6d4ec6dfd40c23ad0b1d7c6bdb208454b1cc64
Mattias Eriksson <ermp82@gmail.com>
2024-03-31 20:49:18 +0200

    Set objects
    Added a function that creates a named object with a dice set. Function
    parameters are set name, number of dies and sides for the dies. The set
    has a .set attribute that is a [] of dies.

--- src/logic/dice.mjs
+++ src/logic/dice.mjs
@@ -5,9 +5,18 @@ export function diceRoll(sides = 20, mod = 0) {
   return Math.round(Math.random() * sides) + mod;
 };
 
-export function dice_sets(name, no_dice, sides) {
+export function createSet(name = "", numDice, sides) {
+  const set = {
+    "name": name,
+    "set": []
+  };
 
-}
+  for (let i = 0; i < numDice; i++) {
+    const dice = createDice(sides);
+    set.set.push(dice);
+  };
+  return set;
+};
 
 export function createDice(sides) {
   const dice = {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 "use strict";
-import { createDice, diceRoll } from "./logic/dice.mjs";
+import { createDice, createSet, diceRoll } from "./logic/dice.mjs";
 
 console.log("Script connection succesful");
 console.log(`First dice roll: ${diceRoll()}`);
 console.log(`Second dice roll with parameter "6": ${diceRoll(6)}`);
 console.log(`Fourth dice roll with parameters "6" and "100" ${diceRoll(6, 100)}`);
 console.log(JSON.stringify(createDice(20)));
+console.log(JSON.stringify((createSet("test", 6, 20))));
