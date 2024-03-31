72a6110146a4242545e22fc6025291a547c9345c
Mattias Eriksson <ermp82@gmail.com>
2024-03-31 22:15:35 +0200

    Code structure
    Modified dice object so that it now creates a dice object that has a
    property .roll() that rolls the dice. Dice.roll(modifier) can be used to
    add a modifier to the outcome.

--- src/logic/dice.mjs
+++ src/logic/dice.mjs
@@ -1,10 +1,13 @@
 "use strict";
 
-
-export function diceRoll(sides = 20, mod = 0) {
-  return Math.round(Math.random() * sides) + mod;
-};
-
+/**
+ * Creates a set of dice with the specified parameters.
+ * @param {string} [name=""] - The name of the set (optional, defaults to an empty string).
+ * @param {number} numDice - The number of dice to create in the set.
+ * @param {number} sides - The number of sides for each dice in the set.
+ * @returns {Object} - The created set object containing dice.
+ * @author Mattias Eriksson
+ */
 export function createSet(name = "", numDice, sides) {
   const set = {
     "name": name,
@@ -18,9 +21,25 @@ export function createSet(name = "", numDice, sides) {
   return set;
 };
 
+/**
+ * Creates a dice object with a specified number of sides.
+ * @param {number} sides - The number of sides for the dice.
+ * @returns {Object} - The dice object with a 'roll' method and 'sides' property.
+ * @author Mattias Eriksson
+ */
 export function createDice(sides) {
-  const dice = {
-    "sides": sides
+  /**
+       * Rolls the dice with an optional modifier and returns the outcome.
+       * @param {number} [modifier=0] - The modifier to be added to the roll result (optional, defaults to 0).
+       * @returns {number} - The outcome of the roll with the modifier.
+       */
+  function roll(modifier = 0) {
+    return Math.floor(Math.random() * sides) + 1 + modifier;
   }
-  return dice;
-};
+
+  return {
+    roll: roll,
+    sides: sides
+  };
+}
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 "use strict";
-import { createDice, createSet, diceRoll } from "./logic/dice.mjs";
+import { createDice, createSet } from "./logic/dice.mjs";
 
-console.log("Script connection succesful");
-console.log(`First dice roll: ${diceRoll()}`);
-console.log(`Second dice roll with parameter "6": ${diceRoll(6)}`);
-console.log(`Fourth dice roll with parameters "6" and "100" ${diceRoll(6, 100)}`);
 console.log(JSON.stringify(createDice(20)));
 console.log(JSON.stringify((createSet("test", 6, 20))));
+
+const dice = createDice(6);
+console.log(dice.roll(dice.sides));
+console.log(dice.sides);
+console.log(dice.roll());
+console.log(dice.roll(100));
