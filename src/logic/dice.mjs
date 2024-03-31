8080302a44121006a745d4a1c8a14fc57f90d42e
Mattias Eriksson <ermp82@gmail.com>
2024-03-31 19:44:38 +0200

    First logic
    Created first function for rolling a dice, the function takes a
    parameter "sides" and rolls a dice with the corresponding number of
    sides and returns the result. Updated roadmap with new milestones. Fixed
    import and export of modules for FireFox browsers.

---
+++ src/logic/dice.mjs
@@ -0,0 +1,7 @@
+"use strict";
+
+
+export function diceRoll(sides = 20) {
+  return Math.round(Math.random() * sides);
+};
+
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
+"use strict";
+import { diceRoll } from "./logic/dice.mjs";
+
 console.log("Script connection succesful");
+console.log(`First dice roll: ${diceRoll()}`);
+console.log(`SEcond dice roll with parameter "6": ${diceRoll(6)}`)
