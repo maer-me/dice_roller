14046841b1f2be4a0fd0cea8f8e21a0c0b431a00
Mattias Eriksson <ermp82@gmail.com>
2024-04-01 09:38:47 +0200

    Cleanup
    General cleanup

--- src/main.js
+++ src/main.js
@@ -1,20 +1,6 @@
 "use strict";
 import { createDice, createSet } from "./logic/dice.mjs";
+import { startMenu } from "./utils/menu.mjs";
 
+startMenu();
 
-const dice = createDice(6);
-console.log(dice.roll(dice.sides));
-console.log(dice.sides);
-console.log(dice.roll());
-console.log(dice.roll(100));
-
-const set = createSet("test", 6, 100);
-console.log(set.name);
-console.log(set.set);
-
-const testSet = [];
-for (let i = 0; i < set.set.length; i++) {
-  testSet.push(set.set[i].roll());
-};
-
-console.log(testSet);
